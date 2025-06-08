2ae88aaf61ac3183c266adb6c756bef4e9995c55
Chamud Sachintha <sheraanmario777@gmail.com>
2025-06-08 17:59:36 +0530

    add cache clear routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ClientInfoController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,3 +23,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('add-new-client', [ClientInfoController::class, 'addNewClinetInfo']);
 Route::post('get-question-list', [QuestionController::class, 'getAllQuestionList']);
 Route::post('submit-results', [QuestionController::class, 'submitQuestionResponse']);
+
+Route::get('/clear-all', function () {
+    Artisan::call('view:clear');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+
+    return response()->json([
+        'status' => 'All caches cleared',
+        'commands' => [
+            'view:clear' => Artisan::output(),
+            'config:clear' => Artisan::output(),
+            'cache:clear' => Artisan::output(),
+        ],
+    ]);
+});
